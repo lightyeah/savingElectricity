f0d8701f86e97340b987645111833ecc0e748d9f
dailuyao <dailuyaome@foxmail.com>
2016-07-14 14:22:05 +0800

    添加了电压 、电流、有效功率、无功功率、视在功率和功率因素六个变量的数据接口，接口函数的具体实现还有待完善。

--- dialog.cpp
+++ dialog.cpp
@@ -1,6 +1,12 @@
 #include "dialog.h"
 #include "ui_dialog.h"
 #include <QString>
+#define BYTE_NUMBER_VOLTAGE 22
+#define BYTE_NUMBER_CURRENT 22
+#define BYTE_NUMBER_EFFECTIVE_POWER
+#define BYTE_NUMBER_REACTIVE_POWER
+#define BYTE_NUMBER_APPRENT_POWER
+#define BYTE_NUMBER_POWER_FACTOR
 
 Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
@@ -10,18 +16,19 @@ Dialog::Dialog(QWidget *parent) :
     portWrite=new QSerialPort(this);
     portRead=new QSerialPort(this);
     initPort();
+    initData();
     portWrite->open(QIODevice::ReadWrite);
     portRead->open(QIODevice::ReadOnly);
-    connect(portRead,&QSerialPort::readyRead,this,&Dialog::getData);
+    connect(portWrite,&QSerialPort::readyRead,this,&Dialog::getData);
 }
 
 void Dialog::initPort()
 {
     portWrite->setPortName(ui->writePortNumber->text());
     portRead->setPortName(ui->readPortNumber->text());
-    portWrite->setBaudRate(QSerialPort::Baud9600);
+    portWrite->setBaudRate(QSerialPort::Baud2400);
     portWrite->setDataBits(QSerialPort::Data8);
-    portWrite->setParity(QSerialPort::NoParity);
+    portWrite->setParity(QSerialPort::EvenParity);
     portWrite->setStopBits(QSerialPort::OneStop);
     portWrite->setFlowControl(QSerialPort::NoFlowControl);
     portRead->setBaudRate(QSerialPort::Baud9600);
@@ -31,6 +38,17 @@ void Dialog::initPort()
     portRead->setFlowControl(QSerialPort::NoFlowControl);
 }
 
+void Dialog::initData()
+{
+    m_data.countApparentPower=0;
+    m_data.countCurrent=0;
+    m_data.countEffectivePower=0;
+    m_data.countPowerFactor=0;
+    m_data.countReactivePower=0;
+    m_data.countVoltage=0;
+    m_readType=NoneType;
+}
+
 Dialog::~Dialog()
 {
     delete ui;
@@ -38,14 +56,89 @@ Dialog::~Dialog()
 
 void Dialog::on_launch_clicked()
 {
-    //    portWrite->setPortName(ui->writePortNumber->text());
-    //    portRead->setPortName(ui->readPortNumber->text());
-    QByteArray data=ui->writePortContent->text().toLocal8Bit();
-    portWrite->write(data.fromHex(data));
+    //    QByteArray data=ui->writePortContent->text().toLocal8Bit();
+    //    portWrite->write(data.fromHex(data));
+    getVoltage();
 }
 
 void Dialog::getData()
 {
-    QByteArray data=portRead->readAll();
-    ui->readPortContent->setText(QString(data.toHex()));
+    switch(m_readType)
+    {
+    case ReadVoltage:
+        m_data.voltage.append(portWrite->readAll());
+        ++m_data.countVoltage;
+        if(m_data.countVoltage>=BYTE_NUMBER_VOLTAGE)
+        {
+            ui->readPortContent->setText(QString(m_data.voltage.toHex()));
+            m_data.countVoltage=0;
+            m_data.voltage.clear();
+        }
+        break;
+    case ReadCurrent:
+        m_data.current.append(portWrite->readAll());
+        ++m_data.countCurrent;
+        if(m_data.countCurrent>=BYTE_NUMBER_CURRENT)
+        {
+            ui->readPortContent->setText(QString(m_data.current.toHex()));
+            m_data.countCurrent=0;
+            m_data.current.clear();
+        }
+        break;
+    case ReadEffectivePower:
+        m_data.effectivePower.append(portWrite->readAll());
+        ++m_data.countEffectivePower;
+        if(m_data.countEffectivePower>=BYTE_NUMBER_EFFECTIVE_POWER)
+        {
+            ui->readPortContent->setText(QString(m_data.effectivePower.toHex()));
+            m_data.countEffectivePower=0;
+            m_data.effectivePower.clear();
+        }
+        break;
+    case ReadReactivePower:
+        m_data.reactivePower.append(portWrite->readAll());
+        ++m_data.countReactivePower;
+        if(m_data.countReactivePower>=BYTE_NUMBER_REACTIVE_POWER)
+        {
+            ui->readPortContent->setText(QString(m_data.reactivePower.toHex()));
+            m_data.countReactivePower=0;
+            m_data.reactivePower.clear();
+        }
+        break;
+    case ReadApparentPower:
+        m_data.apprentPower.append(portWrite->readAll());
+        ++m_data.countApparentPower;
+        if(m_data.countApparentPower>=BYTE_NUMBER_APPRENT_POWER)
+        {
+            ui->readPortContent->setText(QString(m_data.apprentPower.toHex()));
+            m_data.countApparentPower=0;
+            m_data.apprentPower.clear();
+        }
+        break;
+    case ReadPowerFactor:
+        m_data.powerFactor.append(portWrite->readAll());
+        ++m_data.countPowerFactor;
+        if(m_data.countPowerFactor>=BYTE_NUMBER_POWER_FACTOR)
+        {
+            ui->readPortContent->setText(QString(m_data.powerFactor.toHex()));
+            m_data.countPowerFactor=0;
+            m_data.powerFactor.clear();
+        }
+        break;
+    }
+
+
+
+}
+
+void Dialog::getVoltage()
+{
+    QByteArray data(QString("68aaaaaaaaaaaa68110433323435af16").toLocal8Bit());
+    portWrite->write(data.fromHex(data));
+    m_readType=ReadVoltage;
+}
+
+void Dialog::getCurrent()
+{
+
 }
--- dialog.h
+++ dialog.h
@@ -7,6 +7,32 @@ namespace Ui {
 class Dialog;
 }
 
+struct dataToRead
+{
+    int countVoltage;
+    int countCurrent;
+    int countEffectivePower;
+    int countReactivePower;
+    int countApparentPower;
+    int countPowerFactor;
+    QByteArray voltage;
+    QByteArray current;
+    QByteArray effectivePower;
+    QByteArray reactivePower;
+    QByteArray apprentPower;
+    QByteArray powerFactor;
+};
+enum readType
+{
+    NoneType=0,
+    ReadVoltage,
+    ReadCurrent,
+    ReadEffectivePower,
+    ReadReactivePower,
+    ReadApparentPower,
+    ReadPowerFactor
+};
+
 class Dialog : public QDialog
 {
     Q_OBJECT
@@ -14,16 +40,27 @@ class Dialog : public QDialog
 public:
     explicit Dialog(QWidget *parent = 0);
     void initPort();
+    void initData();
+    void getVoltage();
+    void getCurrent();
+    void getEffectivePower();
+    void getReactivePower();
+    void getApparentPower();
+    void getPowerFactor();
     ~Dialog();
 
 private slots:
     void on_launch_clicked();
     void getData();
 
+
 private:
     Ui::Dialog *ui;
     QSerialPort * portWrite;
     QSerialPort * portRead;
+    dataToRead m_data;
+    readType m_readType;
+
 };
 
 #endif // DIALOG_H
