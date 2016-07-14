86869ff908f7d6a1c8aaffeaf3568e35c4829758
dailuyao <dailuyaome@foxmail.com>
2016-07-14 20:53:55 +0800

    完成了全部六个变量的数据解析，下一步需要实现数据的实时绘图

--- dialog.cpp
+++ dialog.cpp
@@ -18,7 +18,7 @@ Dialog::Dialog(QWidget *parent) :
     initPort();
     initData();
     portWrite->open(QIODevice::ReadWrite);
-//    portRead->open(QIODevice::ReadOnly);
+    //    portRead->open(QIODevice::ReadOnly);
     connect(portWrite,&QSerialPort::readyRead,this,&Dialog::getData);
 }
 
@@ -31,11 +31,11 @@ void Dialog::initPort()
     portWrite->setParity(QSerialPort::EvenParity);
     portWrite->setStopBits(QSerialPort::OneStop);
     portWrite->setFlowControl(QSerialPort::NoFlowControl);
-//    portRead->setBaudRate(QSerialPort::Baud9600);
-//    portRead->setDataBits(QSerialPort::Data8);
-//    portRead->setParity(QSerialPort::NoParity);
-//    portRead->setStopBits(QSerialPort::OneStop);
-//    portRead->setFlowControl(QSerialPort::NoFlowControl);
+    //    portRead->setBaudRate(QSerialPort::Baud9600);
+    //    portRead->setDataBits(QSerialPort::Data8);
+    //    portRead->setParity(QSerialPort::NoParity);
+    //    portRead->setStopBits(QSerialPort::OneStop);
+    //    portRead->setFlowControl(QSerialPort::NoFlowControl);
 }
 
 void Dialog::initData()
@@ -58,12 +58,12 @@ void Dialog::on_launch_clicked()
 {
     //    QByteArray data=ui->writePortContent->text().toLocal8Bit();
     //    portWrite->write(data.fromHex(data));
-    getVoltage();
+    //    getVoltage();
 //    getCurrent();
-//    getEffectivePower();
-//    getReactivePower();
-//    getApparentPower();
-//    getPowerFactor();
+    //    getEffectivePower();
+    //    getReactivePower();
+    //    getApparentPower();
+        getPowerFactor();
 }
 
 void Dialog::getData()
@@ -84,8 +84,6 @@ void Dialog::getData()
             QByteArray CL=m_data.voltage.mid(18,1);
             QByteArray CH=m_data.voltage.mid(19,1);
             double C=(CH.toHex().toFloat()-33)*10+(CL.toHex().toFloat()-33)*0.1;
-
-
             ui->readPortContent->setText(QString("%1").arg(C,0));
             m_data.countVoltage=0;
             m_data.voltage.clear();
@@ -96,7 +94,19 @@ void Dialog::getData()
         ++m_data.countCurrent;
         if(m_data.countCurrent>=BYTE_NUMBER_CURRENT)
         {
-            ui->readPortContent->setText(QString(m_data.current.toHex()));
+            QByteArray AL=m_data.current.mid(14,1);
+            QByteArray AM=m_data.current.mid(15,1);
+            QByteArray AH=m_data.current.mid(16,1);
+            double A=(AH.toHex().toFloat()-33)*10+(AM.toHex().toFloat()-33)*0.1+(AL.toHex().toFloat()-33)*0.001;
+            QByteArray BL=m_data.current.mid(17,1);
+            QByteArray BM=m_data.current.mid(18,1);
+            QByteArray BH=m_data.current.mid(19,1);
+            double B=(BH.toHex().toFloat()-33)*10+(BM.toHex().toFloat()-33)*0.1+(BL.toHex().toFloat()-33)*0.001;
+            QByteArray CL=m_data.current.mid(20,1);
+            QByteArray CM=m_data.current.mid(21,1);
+            QByteArray CH=m_data.current.mid(22,1);
+            double C=(CH.toHex().toFloat()-33)*10+(CM.toHex().toFloat()-33)*0.1+(CL.toHex().toFloat()-33)*0.001;
+            ui->readPortContent->setText(QString("%1").arg(A,0));
             m_data.countCurrent=0;
             m_data.current.clear();
         }
@@ -106,7 +116,23 @@ void Dialog::getData()
         ++m_data.countEffectivePower;
         if(m_data.countEffectivePower>=BYTE_NUMBER_EFFECTIVE_POWER)
         {
-            ui->readPortContent->setText(QString(m_data.effectivePower.toHex()));
+            QByteArray SL=m_data.effectivePower.mid(14,1);
+            QByteArray SM=m_data.effectivePower.mid(15,1);
+            QByteArray SH=m_data.effectivePower.mid(16,1);
+            double S=(SH.toHex().toFloat()-33)+(SM.toHex().toFloat()-33)*0.01+(SL.toHex().toFloat()-33)*0.0001;
+            QByteArray AL=m_data.effectivePower.mid(17,1);
+            QByteArray AM=m_data.effectivePower.mid(18,1);
+            QByteArray AH=m_data.effectivePower.mid(19,1);
+            double A=(AH.toHex().toFloat()-33)+(AM.toHex().toFloat()-33)*0.01+(AL.toHex().toFloat()-33)*0.0001;
+            QByteArray BL=m_data.effectivePower.mid(20,1);
+            QByteArray BM=m_data.effectivePower.mid(21,1);
+            QByteArray BH=m_data.effectivePower.mid(22,1);
+            double B=(BH.toHex().toFloat()-33)+(BM.toHex().toFloat()-33)*0.01+(BL.toHex().toFloat()-33)*0.0001;
+            QByteArray CL=m_data.effectivePower.mid(23,1);
+            QByteArray CM=m_data.effectivePower.mid(24,1);
+            QByteArray CH=m_data.effectivePower.mid(25,1);
+            double C=(CH.toHex().toFloat()-33)+(CM.toHex().toFloat()-33)*0.01+(CL.toHex().toFloat()-33)*0.0001;
+            ui->readPortContent->setText(QString("%1").arg(A,0));
             m_data.countEffectivePower=0;
             m_data.effectivePower.clear();
         }
@@ -116,7 +142,23 @@ void Dialog::getData()
         ++m_data.countReactivePower;
         if(m_data.countReactivePower>=BYTE_NUMBER_REACTIVE_POWER)
         {
-            ui->readPortContent->setText(QString(m_data.reactivePower.toHex()));
+            QByteArray SL=m_data.reactivePower.mid(14,1);
+            QByteArray SM=m_data.reactivePower.mid(15,1);
+            QByteArray SH=m_data.reactivePower.mid(16,1);
+            double S=(SH.toHex().toFloat()-33)+(SM.toHex().toFloat()-33)*0.01+(SL.toHex().toFloat()-33)*0.0001;
+            QByteArray AL=m_data.reactivePower.mid(17,1);
+            QByteArray AM=m_data.reactivePower.mid(18,1);
+            QByteArray AH=m_data.reactivePower.mid(19,1);
+            double A=(AH.toHex().toFloat()-33)+(AM.toHex().toFloat()-33)*0.01+(AL.toHex().toFloat()-33)*0.0001;
+            QByteArray BL=m_data.reactivePower.mid(20,1);
+            QByteArray BM=m_data.reactivePower.mid(21,1);
+            QByteArray BH=m_data.reactivePower.mid(22,1);
+            double B=(BH.toHex().toFloat()-33)+(BM.toHex().toFloat()-33)*0.01+(BL.toHex().toFloat()-33)*0.0001;
+            QByteArray CL=m_data.reactivePower.mid(23,1);
+            QByteArray CM=m_data.reactivePower.mid(24,1);
+            QByteArray CH=m_data.reactivePower.mid(25,1);
+            double C=(CH.toHex().toFloat()-33)+(CM.toHex().toFloat()-33)*0.01+(CL.toHex().toFloat()-33)*0.0001;
+            ui->readPortContent->setText(QString("%1").arg(A,0));
             m_data.countReactivePower=0;
             m_data.reactivePower.clear();
         }
@@ -126,7 +168,23 @@ void Dialog::getData()
         ++m_data.countApparentPower;
         if(m_data.countApparentPower>=BYTE_NUMBER_APPRENT_POWER)
         {
-            ui->readPortContent->setText(QString(m_data.apprentPower.toHex()));
+            QByteArray SL=m_data.apprentPower.mid(14,1);
+            QByteArray SM=m_data.apprentPower.mid(15,1);
+            QByteArray SH=m_data.apprentPower.mid(16,1);
+            double S=(SH.toHex().toFloat()-33)+(SM.toHex().toFloat()-33)*0.01+(SL.toHex().toFloat()-33)*0.0001;
+            QByteArray AL=m_data.apprentPower.mid(17,1);
+            QByteArray AM=m_data.apprentPower.mid(18,1);
+            QByteArray AH=m_data.apprentPower.mid(19,1);
+            double A=(AH.toHex().toFloat()-33)+(AM.toHex().toFloat()-33)*0.01+(AL.toHex().toFloat()-33)*0.0001;
+            QByteArray BL=m_data.apprentPower.mid(20,1);
+            QByteArray BM=m_data.apprentPower.mid(21,1);
+            QByteArray BH=m_data.apprentPower.mid(22,1);
+            double B=(BH.toHex().toFloat()-33)+(BM.toHex().toFloat()-33)*0.01+(BL.toHex().toFloat()-33)*0.0001;
+            QByteArray CL=m_data.apprentPower.mid(23,1);
+            QByteArray CM=m_data.apprentPower.mid(24,1);
+            QByteArray CH=m_data.apprentPower.mid(25,1);
+            double C=(CH.toHex().toFloat()-33)+(CM.toHex().toFloat()-33)*0.01+(CL.toHex().toFloat()-33)*0.0001;
+            ui->readPortContent->setText(QString("%1").arg(A,0));
             m_data.countApparentPower=0;
             m_data.apprentPower.clear();
         }
@@ -136,7 +194,19 @@ void Dialog::getData()
         ++m_data.countPowerFactor;
         if(m_data.countPowerFactor>=BYTE_NUMBER_POWER_FACTOR)
         {
-            ui->readPortContent->setText(QString(m_data.powerFactor.toHex()));
+            QByteArray SL=m_data.powerFactor.mid(14,1);
+            QByteArray SH=m_data.powerFactor.mid(15,1);
+            double S=(SH.toHex().toFloat()-33)*0.1+(SL.toHex().toFloat()-33)*0.001;
+            QByteArray AL=m_data.powerFactor.mid(16,1);
+            QByteArray AH=m_data.powerFactor.mid(17,1);
+            double A=(AH.toHex().toFloat()-33)*0.1+(AL.toHex().toFloat()-33)*0.001;
+            QByteArray BL=m_data.powerFactor.mid(18,1);
+            QByteArray BH=m_data.powerFactor.mid(19,1);
+            double B=(BH.toHex().toFloat()-33)*0.1+(BL.toHex().toFloat()-33)*0.001;
+            QByteArray CL=m_data.powerFactor.mid(20,1);
+            QByteArray CH=m_data.powerFactor.mid(21,1);
+            double C=(CH.toHex().toFloat()-33)*0.1+(CL.toHex().toFloat()-33)*0.001;
+            ui->readPortContent->setText(QString("%1").arg(A,0));
             m_data.countPowerFactor=0;
             m_data.powerFactor.clear();
         }
